8cbeb70ef6135479d4b30c28132c4d2961517629
Ford <154796062+4rd2@users.noreply.github.com>
2024-10-30 17:21:00 -0400

    Finished the increase/decrease buttons

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+//Initialzied container constant
+const container = document.querySelector(".container");
+
+//Method that changes the color of the box
+function changeColor(box) {
+    box.style.backgroundColor = "black";
+}
+
+//Uses the changeColor() method to change the color by hovering over a box
+function attachEventListeners() {
+    const rows = container.querySelectorAll(".row");
+    rows.forEach((row) => {
+        const boxes = row.querySelectorAll(".box");
+        boxes.forEach((box) => {
+            box.addEventListener("mouseover", () => {
+                changeColor(box);
+            })
+    })
+});
+}
+
 //Method that changes the grid amount (Ex: 64x64)
 function gridSize(size) {
-    const container = document.querySelector(".container");
+    if (container.hasChildNodes()) {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+    }
     for (let i = 0; i < size; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
@@ -11,24 +36,32 @@ function gridSize(size) {
             row.appendChild(box);
         }
     }
+    attachEventListeners();
 }
-//Method that changes the color of the box
-function changeColor(box) {
-    box.style.backgroundColor = "black";
+gridSize(5);
+//Changes the grid size number at the top with the increase/decrease
+//button
+function gridChange(choice) {
+    const num = document.getElementById('num');
+    if (choice == "Increase") {
+        num.innerHTML++;
+    }
+    else {
+        if (num.innerHTML > 0) {
+            num.innerHTML--;
+        }
+    }
+    gridSize(num.innerHTML);
 }
 
-gridSize(20);
-
-const container = document.querySelector(".container");
-const rows = container.querySelectorAll(".row");
-
-//Uses the changeColor() method to change the color by hovering over a box
-rows.forEach((row) => {
-    const boxes = row.querySelectorAll(".box");
-    boxes.forEach((box) => {
-        box.addEventListener("mouseover", () => {
-            changeColor(box);
-        })
-    })
+//Increase button functionality
+const increase = document.getElementById('increase');
+increase.addEventListener("click", () => {
+    gridChange(increase.innerHTML);
 });
+//Decrease button functionality
+const decrease = document.getElementById('decrease');
+decrease.addEventListener("click", () => {
+    gridChange(decrease.innerHTML);
+})
 
